9e21e80222ac69e9fb66fa82bef966802cd20c24
Leo Shen <i@szclsya.me>
2020-05-16 22:54:44 +0800

    init-elpa: tweak company-mode && add Python language support

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -25,7 +25,11 @@
 
 (use-package company
   :init
-  (global-company-mode))
+  (global-company-mode)
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 1)
+  (setq company-selection-wrap-around t)
+  (company-tng-configure-default))
 
 (use-package lsp-mode
   :hook (XXX-mode . lsp-deferred)
@@ -36,6 +40,11 @@
 (use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
 (use-package lsp-treemacs :commands lsp-treemacs-errors-list)
 
+(use-package company-jedi
+  :config
+  (defun my/python-mode-hook ()
+    (add-to-list 'company-backends 'company-jedi))
+  (add-hook 'python-mode-hook 'my/python-mode-hook))
 
 (use-package rust-mode)
 

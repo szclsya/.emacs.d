349627b4782ce6453603bd622fe6ffb28330d615
Leo Shen <i@szclsya.me>
2020-06-12 17:15:37 +0800

    init-local: set to nil by default

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -2,7 +2,7 @@
 ;;; Commentary: This file is used to set per-machine configs
 ;;; Code:
 
-;; (setq use-chinese-elpa-mirrors t)
+(setq use-chinese-elpa-mirrors nil)
 
 (provide 'init-local)
 ;;; init-local.el ends here

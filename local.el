e9764a69d5426eb1cf604df6f5fab2c89db25f5c
Leo Shen <i@szclsya.me>
2024-09-26 23:24:30 -0400

    add a default local config

---
+++ local.el
@@ -0,0 +1,12 @@
+;;; local.el --- Machine-specific config -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(setq
+ ;; Use Chinese ELPA mirrors or official ELPA
+ use-chinese-elpa-mirrors nil
+ ;; Use emacs-lsp-booster
+ use-emacs-lsp-booster t)
+
+(provide 'local)
+;;; local.el ends here
